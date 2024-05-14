c180e2b1e8e197147143904ae6ef77f57b72151e
Nirajan <ykhatri508@gmail.com>
2024-05-14 17:21:57 +0545

    projects added

--- src/assets/project/projects.js
+++ src/assets/project/projects.js
@@ -4,42 +4,98 @@
   import project4 from "./p4.png";
   import project5 from "./p6.png";
   import project6 from "./p7.png";
+  import mayara1 from "./mayara1.png";
+  import mayara2 from "./mayara2.png";
+  import mayara3 from "./mayara3.png";
+  import mayara4 from "./mayara4.png";
 
   export const projects = [
     {
       id: 1,
       image: project1,
-      name: "Age Calculator",
-      category: "Web Development"
+      images: [mayara1, mayara2, mayara3],
+      name: "UI/UX Desigining",
+      category: "Design",
+      services: "UI/UX Design",
+      link: "http://admin.mayratechnology.com",
+      description: "A complete human resource information system including accounting involves the integration of various aspects of human resource management with accounting principles to track, measure, and report on the financial and non-financial aspects of an organization's employees. This system aims to provide a comprehensive understanding of the costs and values associated with human resources, enabling informed decision-making and strategic planning.",
+      tools: "Adobe Illustrator, Photoshop"
+
     },
     {
       id: 2,
-      image: project2,
-      name: "Credit Card Fraud Detection",
-      category: "Machine learning"
+      image: mayara3,
+      images: [mayara3, mayara1, mayara2, mayara3],
+      name: "Mayara HRIS",
+      category: "Development",
+      services: "Complete human resource information system including accounting",
+      link: "link",
+      description: "A complete human resource information system including accounting involves the integration of various aspects of human resource management with accounting principles to track, measure, and report on the financial and non-financial aspects of an organization's employees. This system aims to provide a comprehensive understanding of the costs and values associated with human resources, enabling informed decision-making and strategic planning.",
+      tools: "React, Tailwind, Redux"
     },
     {
       id: 3,
       image: project3,
-      name: "Agro e-commerce",
-      category: "Graphics Design"
+      images: [mayara1, mayara2, mayara3],
+      name: "Research",
+      category: "Artificial Intelligence",
+      services: "Credit card fraud detection system",
+      link: "http://admin.mayratechnology.com",
+      description: "A complete human resource information system including accounting involves the integration of various aspects of human resource management with accounting principles to track, measure, and report on the financial and non-financial aspects of an organization's employees. This system aims to provide a comprehensive understanding of the costs and values associated with human resources, enabling informed decision-making and strategic planning.",
+      tools: "toolsused"
     },
     {
       id: 4,
       image: project4,
-      name: "User Rating App",
-      category: "Web Development"
+      name: "Illustrations",
+      category: "Design",
+      services: "Graphic Design",
+      link: "http://admin.mayratechnology.com",
+      description: "A complete human resource information system including accounting involves the integration of various aspects of human resource management with accounting principles to track, measure, and report on the financial and non-financial aspects of an organization's employees. This system aims to provide a comprehensive understanding of the costs and values associated with human resources, enabling informed decision-making and strategic planning.",
+      tools: "Adobe Illustrator, Photoshop"
     },
     {
       id: 5,
       image: project5,
-      name: "Mental Health App",
-      category: "Graphics Design"
+      images: [mayara1, mayara2, mayara3],
+      name: "Age Calculator",
+      category: "Development",
+      services: "Calculate the age from a given Date of Birth",
+      link: "http://admin.mayratechnology.com",
+      description: "description",
+      tools: "React, SCSS"
     },
     {
       id: 6,
       image: project6,
+      images: [mayara1, mayara2, mayara3],
+      name: "Business Cards",
+      category: "Web Development",
+      services: "Calculate the age from a given Date of Birth",
+      link: "http://admin.mayratechnology.com",
+      description: "A complete human resource information system including accounting involves the integration of various aspects of human resource management with accounting principles to track, measure, and report on the financial and non-financial aspects of an organization's employees. This system aims to provide a comprehensive understanding of the costs and values associated with human resources, enabling informed decision-making and strategic planning.",
+      tools: "Adobe Illustrator, Photoshop"
+    },
+    {
+      id: 7,
+      image: project6,
+      images: [mayara1, mayara2, mayara3],
+      name: "User rating App",
+      category: "Web Development",
+      services: "Calculate the age from a given Date of Birth",
+      link: "http://admin.mayratechnology.com",
+      description: "A complete human resource information system including accounting involves the integration of various aspects of human resource management with accounting principles to track, measure, and report on the financial and non-financial aspects of an organization's employees. This system aims to provide a comprehensive understanding of the costs and values associated with human resources, enabling informed decision-making and strategic planning.",
+      tools: "React, SCSS"
+    },
+    {
+      id: 8,
+      image: project6,
+      images: [mayara1, mayara2, mayara3],
       name: "FIFA News App",
-      category: "Web Development"
+      category: "Web Development",
+      services: "Here, we can get the daily updates, highlights, scores and FAQ about FIFA World Cup.",
+      link: "lhttp://admin.mayratechnology.comink",
+      description: "A complete human resource information system including accounting involves the integration of various aspects of human resource management with accounting principles to track, measure, and report on the financial and non-financial aspects of an organization's employees. This system aims to provide a comprehensive understanding of the costs and values associated with human resources, enabling informed decision-making and strategic planning.",
+      tools: "React, CSS"
     }
   ];
